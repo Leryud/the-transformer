9216782b954bc05f21086f135b0f27ddee55f95b
Leo <leo@n-vibe.fr>
2023-12-24 15:02:28 +0100

    added position wise feed forward connections

--- src/layers.py
+++ src/layers.py
@@ -163,3 +163,18 @@ class MultiHeadAttention(nn.Module):
         del value
 
         return self.linears[-1](x)
+
+
+class PositionwiseFeedForward(nn.Module):
+    """FFN equation : FFN(x) = max(0, xW_1 + b1)W_2 + b_2"""
+
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff) # xW_1 + b_1
+        self.w_2 = nn.Linear(d_ff, d_model) # xW_2 + b_2
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        # relu is max(0, w_1)
+        return self.w_2(self.dropout(self.w_1(x).relu()))
+
